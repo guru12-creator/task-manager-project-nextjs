9ecb1e750250778f29157dbabae1e6dc51369e3d
guru12-creator <deshmukhgaurav481@gmail.com>
2024-03-15 19:02:17 +0530

    Update route.js

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -2,17 +2,18 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { User } from "@/models/user";
 import { connectDb } from "@/helper/db";
-connectDb();
+
 export async function GET(request) {
   const authToken = request.cookies.get("authToken")?.value;
   console.log(authToken);
-  if(authToken){
+  if( !authToken){
     return NextResponse.json({
       message:"user is not logged in it !!"
     })
   }
   const data = jwt.verify(authToken, process.env.JWT_KEY);
   console.log(data);
+  await connectDb();
   const user = await User.findById(data._id).select("-password");
   //   console.log("test");
   return NextResponse.json(user);
